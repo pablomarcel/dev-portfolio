5c658e1e3972b361697e170334489ab2f2f3db47
pablomarcel <pablomarcel@outlook.com>
2022-08-13 10:08:35 -0700

    first commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Layout from '../components/Layout'
 
+// gatsby
+
 const About = () => {
   return (
     <Layout>
@@ -13,5 +15,5 @@ const About = () => {
     </Layout>
   );
 }
- 
+
 export default About
